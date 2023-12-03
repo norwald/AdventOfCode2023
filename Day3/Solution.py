2e67310725f4615f3d8708706524ffb7bb677380
norwald <v.makariev@gmail.com>
2023-12-03 23:11:43 +0100

    solution for Day3 problem 2

--- Day3/Solution.py
+++ Day3/Solution.py
@@ -1,6 +1,11 @@
 from SolverBase import SolverBase
 
 
+class Gear:
+    def __init__(self, index):
+        self.index = index
+        self.adjacent_parts = []
+
 class EnginePartNumber:
     def __init__(self, value, start_index, end_index):
         self.value = value
@@ -15,9 +20,13 @@ class EngineInstructionLine:
         self.next_instruction_line = None
         self.symbol_indices = []
         self.part_number_values = []
+        self.gears = []
 
         self.init_digit_tracking()
         for counter in range(0, len(string_raw)):
+            if string_raw[counter] == "*":
+                self.gears.append(Gear(counter))
+
             if string_raw[counter] == ".":
                 if self.prev_is_digit:
                     self.update_digit_tracking(counter)
@@ -69,6 +78,28 @@ class EngineInstructionLine:
 
         return False
 
+    def get_valid_gears(self):
+        valid_parts = self.get_valid_part_numbers()
+        for part in valid_parts:
+            for gear in self.gears:
+                if gear.index + 1 == part.start_index or gear.index - 1 == part.end_index:
+                    gear.adjacent_parts.append(part)
+
+        valid_prev_parts = self.prev_instruction_line.get_valid_part_numbers() if self.prev_instruction_line is not None else []
+        for valid_prev_part in valid_prev_parts:
+            for gear in self.gears:
+                if gear.index in range(valid_prev_part.start_index - 1, valid_prev_part.end_index + 2):
+                    gear.adjacent_parts.append(valid_prev_part)
+
+        valid_next_parts = self.next_instruction_line.get_valid_part_numbers() if self.next_instruction_line is not None else []
+        for valid_next_part in valid_next_parts:
+            for gear in self.gears:
+                if gear.index in range(valid_next_part.start_index - 1, valid_next_part.end_index + 2):
+                    gear.adjacent_parts.append(valid_next_part)
+
+        return [gear for gear in self.gears if len(gear.adjacent_parts) == 2]
+
+
 
 class EngineInstruction:
 
@@ -84,6 +115,9 @@ class EngineInstruction:
     def get_valid_instruction_numbers(self):
         return [num for line in self.instruction_lines for num in line.get_valid_part_numbers()]
 
+    def get_valid_gears(self):
+        return [gear for line in self.instruction_lines for gear in line.get_valid_gears()]
+
 
 class Day3Problem1(SolverBase):
 
@@ -93,5 +127,17 @@ class Day3Problem1(SolverBase):
         result = [int(num.value) for num in engine_instructions.get_valid_instruction_numbers()]
         print(sum(result))
 
-test = Day3Problem1("Input.txt")
-test.solve()
+class Day3Problem2(SolverBase):
+
+    def solve(self):
+        instruction_lines = list(map(lambda line: EngineInstructionLine(line), self.input_data))
+        engine_instructions = EngineInstruction(instruction_lines)
+        result = [int(gear.adjacent_parts[0].value) * int(gear.adjacent_parts[1].value) for gear in engine_instructions.get_valid_gears()]
+        print(sum(result))
+
+
+problem1 = Day3Problem1("Input.txt")
+problem1.solve()
+
+problem2 = Day3Problem2("Input.txt")
+problem2.solve()
