0d0f7cb4d01bf784eaeac9676da125377a94e598
norwald <v.makariev@gmail.com>
2023-12-06 01:54:06 +0100

    unsuccessful attempt at problem 2

--- Day5/Solution.py
+++ Day5/Solution.py
@@ -8,10 +8,13 @@ class SparceMapEntry:
         self.source_start = int(split_string[1])
         self.length = int(split_string[2])
 
+        self.destination_end_exclusive = self.destination_start + self.length
+        self.source_end_exclusive = self.source_start + self.length
+
 
 class SparseMap:
     def __init__(self, map_entries, name="none"):
-        self.map_entries = map_entries
+        self.map_entries = sorted(map_entries, key=lambda entry: entry.source_start)
         self.name = name
 
     def get_destination(self, source):
@@ -21,10 +24,54 @@ class SparseMap:
 
         return source
 
+    def get_destination_ranges_for_list(self, source_list):
+        destination_ranges = []
+        for (source_start, source_end_exclusive) in source_list:
+            destination_ranges.extend(self.get_destination_ranges(source_start, source_end_exclusive))
+
+        return destination_ranges
+
+    def get_destination_ranges(self, source_start, source_end_exclusive):
+        destination_ranges = []
+        print("Source Start : {} Source End Exclusive {}".format(source_start, source_end_exclusive))
+        for entry in self.map_entries:
+            if entry.source_start <= source_start:
+                offset = (source_start - entry.source_start)
+                destination_start = entry.destination_start + offset
+                # we are happy entry range contains full lookup range
+                if entry.source_end_exclusive >= source_end_exclusive:
+                    destination_end_exclusive = destination_start + source_end_exclusive - source_start
+                    destination_ranges.append((destination_start, destination_end_exclusive))
+                    source_start = source_end_exclusive
+                    break
+                elif entry.source_end_exclusive > source_start:
+                    destination_end_exclusive = entry.destination_end_exclusive
+                    source_start = entry.source_end_exclusive
+                    if destination_end_exclusive - destination_start > 0:
+                        destination_ranges.append((destination_start, destination_end_exclusive))
+                    continue
+            elif entry.source_start < source_end_exclusive:
+                destination_start = entry.destination_start
+                if entry.source_end_exclusive >= source_end_exclusive:
+                    destination_end_exclusive = destination_start + source_end_exclusive - entry.source_start
+                    destination_ranges.append((destination_start, destination_end_exclusive))
+                    source_start = source_end_exclusive
+                    break
+                else:
+                    destination_end_exclusive = entry.destination_end_exclusive
+                    source_start = entry.source_end_exclusive
+                    if destination_end_exclusive - destination_start > 0:
+                        destination_ranges.append((destination_start, destination_end_exclusive))
+                    continue
+
+        if source_end_exclusive - source_start >= 1:
+            destination_ranges.append((source_start, source_end_exclusive))
+
+        return destination_ranges
 
 class SeedsMapper:
 
-    def __init__(self, input_lines):
+    def __init__(self, input_lines, seeds_parser=None):
         self.seeds = []
         self.seed_to_soil = None
         self.soil_to_fertilizer = None
@@ -35,16 +82,18 @@ class SeedsMapper:
         self.humidity_to_location = None
         self.all_maps = []
 
-        self.parse_input([line for line in input_lines if line.strip()])
+        seeds_parser = seeds_parser if seeds_parser is not None else lambda current_seeds: [int(seed) for seed in
+                                                                                            current_seeds]
+        self.parse_input([line for line in input_lines if line.strip()], seeds_parser)
 
-    def parse_input(self, input_lines):
+    def parse_input(self, input_lines, seeds_parser):
         counter = 0
         while counter < len(input_lines):
             current_line = input_lines[counter]
 
             if current_line.startswith("seeds"):
                 current_seeds = current_line.split(":")[1].strip().split()
-                self.seeds.extend([int(seed) for seed in current_seeds])
+                self.seeds.extend(seeds_parser(current_seeds))
                 counter += 1
 
             elif current_line.startswith("seed-to-soil map"):
@@ -96,7 +145,6 @@ class SeedsMapper:
                 self.all_maps.append(self.humidity_to_location)
                 counter = end_index
 
-
     @staticmethod
     def create_map(lines):
         return SparseMap([SparceMapEntry(line) for line in lines])
@@ -114,11 +162,18 @@ class SeedsMapper:
         destination = seed
         for current_map in self.all_maps:
             destination = current_map.get_destination(destination)
-            #print("{} {}".format(current_map.name, destination))
         return destination
 
     def get_all_locations(self):
         return [self.get_location(seed) for seed in self.seeds]
+
+    def get_location_by_range(self, seed_list):
+        destinations = seed_list
+        for counter in range(0, len(self.all_maps)):
+            destinations = self.all_maps[counter].get_destination_ranges_for_list(destinations)
+
+        return destinations
+
 
 class Problem1(SolverBase):
 
@@ -127,5 +182,47 @@ class Problem1(SolverBase):
         seeds_mapper = SeedsMapper(input_lines)
         print(min(seeds_mapper.get_all_locations()))
 
-test = Problem1("Input.txt")
-test.solve()
+
+class Problem2(SolverBase):
+
+    def solve(self):
+        input_lines = self.input_data
+        seeds_mapper = SeedsMapper(input_lines, seeds_parser=lambda seeds: [])
+
+        current_seeds = self.input_data[0].split(":")[1].strip().split()
+        counter = 0
+        seed_ranges = []
+        while counter < len(current_seeds):
+            seed_start = int(current_seeds[counter])
+            length = int(current_seeds[counter + 1])
+            seed_ranges.append((seed_start, seed_start + length))
+            counter += 2
+
+        print(seeds_mapper.get_location_by_range(seed_ranges))
+        # print(min(seeds_mapper.get_all_locations()))
+
+    # brute force standard calculation which does not work
+    @staticmethod
+    def parse_seed_ranges(current_seeds):
+        all_seeds = []
+
+        counter = 0
+        while counter < len(current_seeds):
+            seed_start = int(current_seeds[counter])
+            length = int(current_seeds[counter + 1])
+            for seed in range(seed_start, seed_start + length):
+                all_seeds.append(seed)
+
+            counter += 2
+
+        return all_seeds
+
+
+# problem1 = Problem1("TestInput.txt")
+# problem1.solve()
+
+problem2 = Problem2("TestInput.txt")
+problem2.solve()
+
+#test = SparseMap([SparceMapEntry("20 10 5"),  SparceMapEntry("30 20 5") ])
+#print(test.get_destination_ranges(1 ,9))
